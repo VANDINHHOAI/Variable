b9ad252430ab6c7d3c87294bb4e7da22bf80f62c
VANDINHHOAI <115123521+VANDINHHOAI@users.noreply.github.com>
2022-10-06 10:04:52 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,30 @@
+package basic.dev;
+/**
+ * 
+ * @author HP
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Khai báo biến
+		 */
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va ten");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14F;
+	}
+
+}
